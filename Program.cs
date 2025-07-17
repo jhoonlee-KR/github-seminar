5c6b4012d9a03c698b0f1c97e913170dc5f3b537
jhoonlee-KR <jhoonlee@krs.co.kr>
2025-07-17 10:13:57 +0900

    add

--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@
     {
         Console.WriteLine("Hello, .NET Core!");
 
-        // Add your application logic here
+        // Add your application glogic here
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
         // End of application logic
         Console.WriteLine("Goodbye!");
         // Ensure the application exits cleanly
         Environment.Exit(0);
+
+        add
  }
  }
